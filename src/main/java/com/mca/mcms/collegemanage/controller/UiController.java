f0e2c29a634f1960baa81ced1e356a36b4034410
SriMani-7 <basinenisrimanikanta@gmail.com>
2023-06-09 11:47:46 +0530

    Fix caching issue in Spring controller endpoints
    Updated the Spring controller endpoints in the UiController class to address the caching problem. Added cache control headers to the HTTP response, specifically setting "Cache-Control" to "no-cache, no-store, must-revalidate".

--- src/main/java/com/mca/mcms/collegemanage/controller/UiController.java
+++ src/main/java/com/mca/mcms/collegemanage/controller/UiController.java
@@ -2,6 +2,7 @@ package com.mca.mcms.collegemanage.controller;
 
 import com.mca.mcms.collegemanage.Auth;
 import com.mca.mcms.collegemanage.entity.UserType;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,34 +11,44 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class UiController {
 
     @GetMapping("admin")
-    public String adminDashboard(HttpSession httpSession) {
+    public String adminDashboard(HttpSession httpSession, HttpServletResponse response) {
+        applyHeaders(response);
         return Auth.hasLogin(UserType.ADMIN, httpSession) ? "admin/dashboard.html" : "error/403.html";
     }
 
     @GetMapping("admin-courses")
-    public String adminCourses(HttpSession httpSession) {
+    public String adminCourses(HttpSession httpSession, HttpServletResponse response) {
+        applyHeaders(response);
         return Auth.hasLogin(UserType.ADMIN, httpSession) ? "admin/courses.html" : "error/403.html";
     }
 
 
     @GetMapping("student")
-    public String studentDashboard(HttpSession httpSession) {
+    public String studentDashboard(HttpSession httpSession, HttpServletResponse response) {
+        applyHeaders(response);
         return Auth.hasLogin(UserType.STUDENT, httpSession) ? "student/dashboard.html" : "error/403.html";
     }
 
     @GetMapping("faculty")
-    public String facultyDashboard(HttpSession httpSession) {
+    public String facultyDashboard(HttpSession httpSession, HttpServletResponse response) {
+        applyHeaders(response);
         return Auth.hasLogin(UserType.FACULTY, httpSession) ? "faculty/dashboard.html" : "error/403.html";
     }
 
     @GetMapping("faculty-register")
-    public String facultyRegister(HttpSession httpSession) {
+    public String facultyRegister(HttpSession httpSession, HttpServletResponse response) {
+        applyHeaders(response);
         return Auth.hasLogin(UserType.ADMIN, httpSession) ? "admin/faculty-register.html" : "error/403.html";
     }
 
     @GetMapping("student-register")
-    public String studentRegister(HttpSession httpSession) {
+    public String studentRegister(HttpSession httpSession, HttpServletResponse response) {
+        applyHeaders(response);
         return Auth.hasLogin(UserType.ADMIN, httpSession) ? "admin/student-register.html" : "error/403.html";
+    }
+
+    private void applyHeaders(HttpServletResponse response) {
+        response.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
     }
 
 }
