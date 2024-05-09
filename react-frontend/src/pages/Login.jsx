18728179475cae6f38eeec9773666b6ee74e3f9e
SriMani-7 <basinenisrimanikanta@gmail.com>
2024-05-09 18:12:21 +0530

    shadcn components in login page

--- react-frontend/src/pages/Login.jsx
+++ react-frontend/src/pages/Login.jsx
@@ -1,8 +1,78 @@
-import { useState } from "react";
-import { BaseLinkButton, PrimaryButton } from "../ui/buttons";
-import { InputTextField } from "../ui/input";
-import { PasswordToggle, ToggleButton } from "../ui/toggle";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form";
+import { useForm } from "react-hook-form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+
+export default function LoginPage() {
+  const navigate = useNavigate();
+  const loginform = useForm();
+
+  const handleSubmit = (data) => {
+    console.log(data);
+    localStorage.setItem("userType", data.userType);
+
+    // TODO: Implement login logic here i am approving authentication without validation fortesting.
+    // let data = Object.fromEntries(formData.entries());
+
+    navigate("/" + data.userType.toLowerCase());
+  };
+
+  return (
+    <div className="container mx-auto flex items-center justify-center min-h-screen flex-col text-center">
+      <h3 className=" text-3xl mb-12">Signin into</h3>
+      <Form {...loginform}>
+        <form
+          method="post"
+          onSubmit={loginform.handleSubmit(handleSubmit)}
+          className="flex flex-col w-8/12 lg:w-4/12 text-left gap-2"
+        >
+          <UserTypeField control={loginform.control} />
+          <FormField
+            control={loginform.control}
+            name="userName"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Username</FormLabel>
+                <FormControl>
+                  <Input placeholder="jhon doe" {...field} />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={loginform.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input type="password" {...field} />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          <Button size="lg" type="submit" className="mt-6">
+            Login
+          </Button>
+          <Link
+            to="/forgetpassowrd"
+            className=" px-4 py-3 text-center bg-secondary text-secondary-foreground hover:bg-secondary/80"
+          >
+            Forget password?
+          </Link>
+        </form>
+      </Form>
+    </div>
+  );
+}
 
 const USER_TYPES = [
   { label: "Student", value: "STUDENT" },
@@ -10,63 +80,34 @@ const USER_TYPES = [
   { label: "Staff", value: "ADMIN" },
 ];
 
-export default function LoginPage() {
-  const [passwordShown, setPasswordShown] = useState(false);
-  const [userType, setUserType] = useState("STUDENT");
-  const navigate = useNavigate();
-
-  const navigateForgetPassword = () => navigate("/forgetpassowrd")
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    let formData = new FormData(event.target)
-    localStorage.setItem("userType", formData.get('userType'));
-
-    // TODO: Implement login logic here i am approving authentication without validation fortesting.
-    // let data = Object.fromEntries(formData.entries());
-
-    navigate("/"+formData.get('userType').toLowerCase())
-  }
-
+function UserTypeField({ control }) {
   return (
-    <div className="container mx-auto flex items-center justify-center min-h-screen flex-col text-center">
-      <h3 className=" text-3xl mb-12">Signin into</h3>
-      <form method="post" className="flex flex-col w-8/12 lg:w-4/12" onSubmit={handleSubmit}>
-      
-        <fieldset className="grid grid-cols-3 gap-4">
-          {USER_TYPES.map((ut) => (
-            <ToggleButton
-              name="userType"
-              key={ut.value}
-              value={ut.value}
-              onChange={(event) => setUserType(event.target.value)}
-              id={ut.value}
-              isChecked={ut.value === userType}
+    <FormField
+      control={control}
+      name="userType"
+      render={({ field }) => (
+        <FormItem className="mb-3">
+          <FormControl>
+            <RadioGroup
+              onValueChange={field.onChange}
+              defaultValue={field.value}
+              className="flex flex-col"
             >
-              <p className="text-gray-700">{ut.label}</p>
-            </ToggleButton>
-          ))}
-        </fieldset>
-
-        <InputTextField name="userName" type="text" label="User name" />
-        <InputTextField
-          name="password"
-          type={passwordShown ? "password" : "text"}
-          label="Password"
-          trailingContent=<PasswordToggle
-            show={passwordShown}
-            onChange={setPasswordShown}
-          />
-        />
-        <div className="h-8"></div>
-        <PrimaryButton>
-          <span>Login</span>
-        </PrimaryButton>
-        <div className="h-6"></div>
-        <BaseLinkButton handleOnClick={navigateForgetPassword}>
-          <span>Forget Password</span>
-        </BaseLinkButton>
-      </form>
-    </div>
+              {USER_TYPES.map((ut) => (
+                <FormItem
+                  className="flex flex-row gap-3 items-center py-1 justify-between"
+                  key={ut.value}
+                >
+                  <FormLabel>{ut.label}</FormLabel>
+                  <FormControl>
+                    <RadioGroupItem value={ut.value} />
+                  </FormControl>
+                </FormItem>
+              ))}
+            </RadioGroup>
+          </FormControl>
+        </FormItem>
+      )}
+    />
   );
 }
