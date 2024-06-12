18411662d05091e375cdf652806ac6def807d30e
SriMani-7 <basinenisrimanikanta@gmail.com>
2024-06-12 18:02:40 +0530

    creating new programme

--- spring-backend/src/main/java/com/mca/mcms/collegemanage/controller/ProgrammeController.java
+++ spring-backend/src/main/java/com/mca/mcms/collegemanage/controller/ProgrammeController.java
@@ -1,13 +1,26 @@
 package com.mca.mcms.collegemanage.controller;
 
+import com.fasterxml.jackson.core.JsonFactoryBuilder;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mca.mcms.collegemanage.dto.ProgrammeDto;
+import com.mca.mcms.collegemanage.entity.Department;
+import com.mca.mcms.collegemanage.entity.UgDegree;
+import com.mca.mcms.collegemanage.entity.UgProgramme;
+import com.mca.mcms.collegemanage.projection.DegreeDto;
 import com.mca.mcms.collegemanage.repo.DepartmentRepository;
 import com.mca.mcms.collegemanage.repo.UgDegreeRepository;
 import com.mca.mcms.collegemanage.repo.UgProgrammeRepository;
+import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.*;
 
 @Controller
 @RequestMapping("/api/programmes")
@@ -29,8 +42,47 @@ public class ProgrammeController {
         return ResponseEntity.ofNullable(degreeRepository.findAllDegrees());
     }
 
+    @PostMapping
+    public ResponseEntity<?> createProgramme(@RequestBody ProgrammeDto programmeDto) {
+        // Validate and Retrieve the degree from the database
+        Optional<UgDegree> degreeOptional = degreeRepository.findById(programmeDto.getDegree());
+        if (degreeOptional.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid degree code: " + programmeDto.getDegree());
+        }
+
+        // Create the UgProgramme entity
+        UgProgramme ugProgramme = new UgProgramme(programmeDto.getCode(), programmeDto.getName());
+        ugProgramme.setUgDegree(degreeOptional.get());
+
+        // Retrieve and associate the departments
+        List<Department> departments = new ArrayList<>();
+        for (ProgrammeDto.Department departmentDto : programmeDto.getDepartments()) {
+            Optional<Department> departmentOptional = departmentRepository.findById(departmentDto.getId());
+            if (departmentOptional.isEmpty()) {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid department id: " + departmentDto.getId());
+            }
+            departments.add(departmentOptional.get());
+        }
+        ugProgramme.setCoreDepartments(departments);
+
+        // Save the UgProgramme entity
+        UgProgramme savedProgramme = programmeRepository.save(ugProgramme);
+        var map = new HashMap<String, Object>();
+        map.put("code", savedProgramme.getCode());
+        map.put("name", savedProgramme.getName());
+        map.put("degree", degreeOptional.get().getCode());
+        map.put("departments", savedProgramme
+                .getCoreDepartments()
+                .stream()
+                .map(department ->
+                        Map.of("id", department.getId(), "name", department.getName())
+                ).toList()
+        );
+        return ResponseEntity.ok(map);
+    }
+
     @GetMapping("/departments")
-    private ResponseEntity<?> getAvailableDepartments() {
+    public ResponseEntity<?> getAvailableDepartments() {
         return ResponseEntity.ofNullable(departmentRepository.getAvailableDepts());
     }
 
--- spring-backend/src/main/java/com/mca/mcms/collegemanage/entity/UgProgramme.java
+++ spring-backend/src/main/java/com/mca/mcms/collegemanage/entity/UgProgramme.java
@@ -1,25 +1,39 @@
 package com.mca.mcms.collegemanage.entity;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.List;
 
-@Data
 @Entity
 @Table(name = "ug_programmes")
+@NoArgsConstructor
+@Getter
+@Setter
 public class UgProgramme {
     @Id
     private String code;
     private String name;
     @JoinColumn(name = "degree_code", referencedColumnName = "code", nullable = false, foreignKey = @ForeignKey(name = "fk_programme_degree_code"))
-    @ManyToOne(cascade = CascadeType.ALL)
+    @ManyToOne(cascade = CascadeType.ALL,fetch = FetchType.LAZY)
+    @JsonBackReference
     private UgDegree ugDegree;
     @JoinTable(
             name = "ug_programme_core_departments",
             foreignKey = @ForeignKey(name = "fk_programme_core_department_code"),
             inverseForeignKey = @ForeignKey(name = "fk_department_programme")
     )
-    @ManyToMany(cascade = CascadeType.ALL)
+    @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JsonManagedReference
     private List<Department> coreDepartments;
+
+    public UgProgramme(String code, String name) {
+        this.code = code;
+        this.name = name;
+    }
 }
