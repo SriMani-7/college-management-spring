7ef8f5d24bb92a93e20dfabeae41021a5b5a6ef4
SriMani-7 <basinenisrimanikanta@gmail.com>
2024-06-12 11:50:53 +0530

    listing all available departments for creating new programme

--- spring-backend/src/main/java/com/mca/mcms/collegemanage/controller/ProgrammeController.java
+++ spring-backend/src/main/java/com/mca/mcms/collegemanage/controller/ProgrammeController.java
@@ -1,5 +1,6 @@
 package com.mca.mcms.collegemanage.controller;
 
+import com.mca.mcms.collegemanage.repo.DepartmentRepository;
 import com.mca.mcms.collegemanage.repo.UgDegreeRepository;
 import com.mca.mcms.collegemanage.repo.UgProgrammeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,16 +15,23 @@ public class ProgrammeController {
 
     private final UgProgrammeRepository programmeRepository;
     private final UgDegreeRepository degreeRepository;
+    private final DepartmentRepository departmentRepository;
 
     @Autowired
-    public ProgrammeController(UgProgrammeRepository programmeRepository, UgDegreeRepository degreeRepository) {
+    public ProgrammeController(UgProgrammeRepository programmeRepository, UgDegreeRepository degreeRepository, DepartmentRepository departmentRepository) {
         this.programmeRepository = programmeRepository;
         this.degreeRepository = degreeRepository;
+        this.departmentRepository = departmentRepository;
     }
 
     @GetMapping
     public ResponseEntity<?> getProgrammes() {
         return ResponseEntity.ofNullable(degreeRepository.findAllDegrees());
+    }
+
+    @GetMapping("/departments")
+    private ResponseEntity<?> getAvailableDepartments() {
+        return ResponseEntity.ofNullable(departmentRepository.getAvailableDepts());
     }
 
 }
--- spring-backend/src/main/java/com/mca/mcms/collegemanage/repo/DepartmentRepository.java
+++ spring-backend/src/main/java/com/mca/mcms/collegemanage/repo/DepartmentRepository.java
@@ -1,10 +1,17 @@
 package com.mca.mcms.collegemanage.repo;
 
 import com.mca.mcms.collegemanage.entity.Department;
+import com.mca.mcms.collegemanage.projection.DegreeDto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface DepartmentRepository extends JpaRepository<Department, Long> {
     boolean existsByName(String name);
+
+    @Query("select d.id as id, d.name as name from Department d")
+    List<DegreeDto.DepartmentDto> getAvailableDepts();
 }
