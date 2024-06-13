b1bbf27c74af7fe6e851bdf3d22177349911a5ca
SriMani-7 <basinenisrimanikanta@gmail.com>
2024-06-13 21:34:54 +0530

    Creating and reading faculty under the department
    (cherry picked from commit 0691ab5d23f26735bb569b5e0763d20bad23380b)

---
+++ src/main/java/com/mca/mcms/collegemanage/dto/FacultyDto.java
@@ -0,0 +1,15 @@
+package com.mca.mcms.collegemanage.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class FacultyDto {
+    private String name;
+    private String email;
+    private String address;
+    private String gender;
+    private String designation;
+    private long mobileNumber;
+}
