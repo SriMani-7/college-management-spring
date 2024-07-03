dcf0c958b1f30a3016b90e9c82fa6a5e921cb8af
SriMani-7 <basinenisrimanikanta@gmail.com>
2024-07-03 11:09:37 +0530

    AdmissionForm has sperate file

--- react-frontend/src/features/admissions/index.jsx
+++ react-frontend/src/features/admissions/index.jsx
@@ -1,78 +1,9 @@
-import { Button } from "@/components/ui/button";
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-} from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
+import { Form } from "@/components/ui/form";
 import { Toaster } from "@/components/ui/toaster";
 import { toast } from "@/components/ui/use-toast";
 import { useForm } from "react-hook-form";
-
-const AdmissionForm = ({ control, onSubmit }) => {
-  const AdmiFormField = ({ name, label, type = "text" }) => (
-    <FormField
-      control={control}
-      name={name}
-      render={({ field }) => (
-        <FormItem>
-          <FormLabel className=" font-normal">{label}</FormLabel>
-          <FormControl>
-            <Input type={type} {...field} />
-          </FormControl>
-        </FormItem>
-      )}
-    />
-  );
-  return (
-    <form onSubmit={onSubmit} method="post" className=" space-y-3">
-      <h4 className=" text-xl mb-4 font-medium">Student details</h4>
-      <hr />
-      <fieldset>
-        <div className="flex gap-3">
-          <AdmiFormField name="firstName" label="First Name" />
-          <AdmiFormField name="lastName" label="Last Name" />
-        </div>
-        <div className="flex gap-3">
-          <AdmiFormField name="mobile" label="Mobile Number" />
-          <AdmiFormField name="email" label="Email Address" />
-        </div>
-        <AdmiFormField name="region" label="Region" />
-        <AdmiFormField name="address" label="Address" />
-        <AdmiFormField name="city" label="City" />
-        <div className="flex gap-3">
-          <AdmiFormField name="state" label="state" />
-          <AdmiFormField name="pincode" label="Pincode" />
-        </div>
-      </fieldset>
-      <h4 className=" text-xl mb-4 font-medium">Education details</h4>
-      <hr />
-      <fieldset>
-        <div className="flex gap-3">
-          <AdmiFormField name="xiiHallticket" label="Holl ticket number" />
-          <AdmiFormField name="xiiMarks" label="Marks" />
-        </div>
-        <AdmiFormField name="xiiCollege" label="College Name" />
-        <AdmiFormField name="xiiCollegeLocation" label="Location" />
-        <AdmiFormField name="xiiCourse" label="Course" />
-        <div className="flex gap-3">
-          <AdmiFormField name="xiiType" label="Course completion type" />
-          <AdmiFormField name="xiiYear" label="Passout year" />
-        </div>
-      </fieldset>
-      <h4 className=" text-xl mb-4 font-medium">Course details</h4>
-      <fieldset>
-        <AdmiFormField name="degree" label="Degree type" />
-        <AdmiFormField name="programmeCode" label="Programme" />
-        <AdmiFormField name="acadamicYear" label="Acadamic Year" />
-        <AdmiFormField name="admissionNumber" label="Admission number" />
-      </fieldset>
-      <Button type="submit">Submit</Button>
-    </form>
-  );
-};
+import { AdmissionForm } from "./form";
+import { createStudent } from "./services";
 
 export default function ApplicationFormPage() {
   const { control, ...rest } = useForm();
