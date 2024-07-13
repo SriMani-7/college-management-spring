ac3a373360130b36461fbe3d5ad8640efe59ece1
SriMani-7 <basinenisrimanikanta@gmail.com>
2024-07-13 11:16:14 +0530

    Department controller & repository removed

--- spring-backend/src/main/java/com/mca/mcms/collegemanage/repo/FacultyRepository.java
+++ spring-backend/src/main/java/com/mca/mcms/collegemanage/repo/FacultyRepository.java
@@ -3,8 +3,5 @@ package com.mca.mcms.collegemanage.repo;
 import com.mca.mcms.collegemanage.entity.Faculty;
 import org.springframework.data.repository.ListCrudRepository;
 
-import java.util.List;
-
 public interface FacultyRepository extends ListCrudRepository<Faculty, Long> {
-    List<Faculty> findAllByDepartmentId(long did);
 }
