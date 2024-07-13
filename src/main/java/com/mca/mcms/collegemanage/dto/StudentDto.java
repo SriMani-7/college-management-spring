a22003ff72ac9959a4978f57be4924a123771641
SriMani-7 <basinenisrimanikanta@gmail.com>
2024-07-13 11:24:27 +0530

    api: UgProgramme, UgDegree and it's related classes are removed

--- src/main/java/com/mca/mcms/collegemanage/dto/StudentDto.java
+++ src/main/java/com/mca/mcms/collegemanage/dto/StudentDto.java
@@ -1,45 +1,20 @@
 package com.mca.mcms.collegemanage.dto;
 
-import com.mca.mcms.collegemanage.entity.embed.EducationXii;
-import com.mca.mcms.collegemanage.entity.embed.StudentAddress;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-import org.springframework.beans.BeanUtils;
 
 @Getter
 @Setter
 @ToString
 public class StudentDto {
     private long admissionNumber;
-    private String academicYear;
     private String address;
     private String city;
     private String email;
     private String firstName;
     private String lastName;
     private String mobile;
-    private String programme;
-    private String region;
     private String state;
     private int pincode;
-    private long xiiHallticket;
-    private String xiiCollege;
-    private String xiiCollegeLocation;
-    private String xiiCourse;
-    private int xiiMarks;
-    private String xiiType;
-    private int xiiYear;
-
-    public EducationXii getEducation() {
-        var xii = new EducationXii();
-        BeanUtils.copyProperties(this, xii);
-        return xii;
-    }
-
-    public StudentAddress getStudentAddress() {
-        var address = new StudentAddress();
-        BeanUtils.copyProperties(this, address);
-        return address;
-    }
 }
--- src/main/java/com/mca/mcms/collegemanage/entity/Student.java
+++ src/main/java/com/mca/mcms/collegemanage/entity/Student.java
@@ -1,7 +1,5 @@
 package com.mca.mcms.collegemanage.entity;
 
-import com.mca.mcms.collegemanage.entity.embed.EducationXii;
-import com.mca.mcms.collegemanage.entity.embed.StudentAddress;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,30 +15,18 @@ public class Student {
     @Column(name = "admission_number")
     private long admissionNumber;
 
-    public Student(long admissionNumber, String academicYear, String email, String firstName, String lastName, String region) {
+    public Student(long admissionNumber, String email, String firstName, String lastName) {
         this.admissionNumber = admissionNumber;
-        this.academicYear = academicYear;
         this.email = email;
         this.firstName = firstName;
         this.lastName = lastName;
-        this.region = region;
     }
 
-    @Column(name = "academic_year")
-    private String academicYear;
     @Column(unique = true)
     private String email;
     @Column(name = "first_name")
     private String firstName;
     @Column(name = "last_name")
     private String lastName;
-    @JoinColumn(foreignKey = @ForeignKey(name = "fk_student_programme_code"))
-    @ManyToOne(cascade = CascadeType.ALL)
-    private UgProgramme ugProgramme;
-    private String region;
-    @Embedded
-    private EducationXii educationXii;
-    @Embedded
-    private StudentAddress address;
 
 }
